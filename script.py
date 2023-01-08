f0fb033d0b28d269f61d642be4692b627ea5aa89
Schwartz, Billy <Billy.Schwartz@sscinc.com>
2023-01-08 17:49:18 -0500

    writing to the test.txt file

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
 import os
 
 os.system('touch ./test.txt')
+
+##open the file
+file = open('test.txt', 'w')
+instance_name = os.environ.get("instance_name")
+##write to the file
+file.write(f'this instance is {instance_name}')
+##close the file
+file.close()
